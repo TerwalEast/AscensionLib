92327523e73fb0dca77cfc666af6f45cfe2b8aa8
TerwalEast <terwaleast@gmail.com>
2021-07-12 15:27:31 +0800

    Add new files

---
+++ AscensionLib/include/AL_Scene.hpp
@@ -0,0 +1,14 @@
+//
+//  AL_Scene.hpp
+//  TestGame
+//
+//  Created by shan on 2021/7/8.
+//  Copyright © 2021 TeamAscension. All rights reserved.
+//
+
+#ifndef AL_Scene_hpp
+#define AL_Scene_hpp
+
+#include <stdio.h>
+
+#endif /* AL_Scene_hpp */
---
+++ AscensionLib/src/AL_Scene.cpp
@@ -0,0 +1,12 @@
+//
+//  AL_Scene.cpp
+//  TestGame
+//
+//  Created by shan on 2021/7/8.
+//  Copyright © 2021 TeamAscension. All rights reserved.
+//
+
+#include "../include/AL_Scene.hpp"
+
+
+
